e9c3b57d8d3d7f3f82f450209395f697c6e65f4c
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-11-09 18:03:47 +0530

    Create Sol_SAA_2.py

---
+++ Sol_SAA_2.py
@@ -0,0 +1,10 @@
+dict1={"Mumbai":11,"Delhi":22,"Bangalore":33,"Hyderabad":44}
+print("The original dictionary is :\n", dict1)
+
+print("\nThe keys of dictionary are :\n", dict1.keys())
+
+print("\nThe values inside the key-value pair are :\n", dict1.values())
+
+print("\nThe pin code of Mumbai is :", dict1["Mumbai"])
+
+print("\nThe pin code of Delhi is :", dict1["Delhi"])
